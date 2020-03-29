1e2b0efde0762f3202d5c967a2b7a4ce58ebb60e
Maciek <maciekpiszek@wp.pl>
2020-03-29 18:14:38 +0200

    Code for APBD cw2

---
+++ Cw2/Controllers/StudentsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw2.DAL;
+using Cw2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw2.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            else if (id == 3)
+            {
+                return Ok("Andrzejewski");
+            }
+            return NotFound("Nie znaleziono studenta!");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id)
+        {
+            return Ok("Aktualizacja dokończona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult RemoveStudent(int id)
+        {
+            return Ok("Usuwanie ukończone");
+        }
+    }
+}
---
+++ Cw2/DAL/IDbService.cs
@@ -0,0 +1,13 @@
+﻿using Cw2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw2.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
